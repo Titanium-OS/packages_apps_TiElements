cf0daf5191a2dd339a4e2151a730cbec8e0a8daf
Varun Date <date.varun123@gmail.com>
2020-09-01 04:58:58 +1000

    TiElements: Disable Force FP authentication preference if encrypted
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- src/com/titanium/tielements/fragments/Lockscreen.java
+++ src/com/titanium/tielements/fragments/Lockscreen.java
@@ -30,10 +30,12 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
 
 import com.android.settings.R;
+import com.android.internal.widget.LockPatternUtils;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.titanium.fod.FodUtils;
 import com.android.settings.Utils;
+import com.titanium.support.preferences.*;
 
 public class Lockscreen extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
@@ -41,8 +43,11 @@ public class Lockscreen extends SettingsPreferenceFragment implements
     private static final String TAG = "Lockscreen";
 
     private static final String FOD_ICON_PICKER_CATEGORY = "fod_icon_picker";
+    private static final String FP_KEYSTORE = "fp_unlock_keystore";
 
     private PreferenceCategory mFODIconPickerCategory;
+    private SwitchPreference mFingerprintVib;
+    private SystemSettingSwitchPreference mFingerprintUnlock;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,6 +62,18 @@ public class Lockscreen extends SettingsPreferenceFragment implements
         mFODIconPickerCategory = findPreference(FOD_ICON_PICKER_CATEGORY);
         if (mFODIconPickerCategory != null && !FodUtils.hasFodSupport(getContext())) {
             prefScreen.removePreference(mFODIconPickerCategory);
+        }
+
+        mFingerprintUnlock = (SystemSettingSwitchPreference) findPreference(FP_KEYSTORE);
+
+        if (mFingerprintUnlock != null) {
+           if (LockPatternUtils.isDeviceEncryptionEnabled()) {
+               mFingerprintUnlock.setEnabled(false);
+               mFingerprintUnlock.setSummary(R.string.fp_encrypt_warning);
+            } else {
+               mFingerprintUnlock.setEnabled(true);
+               mFingerprintUnlock.setSummary(R.string.fp_unlock_keystore_summary);
+            }
         }
     }
 
